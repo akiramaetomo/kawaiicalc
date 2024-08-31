dca0e8a86e5c2fdd5fe9dea5b6967bcc4bca3075
Akira Maetomo <akiramaetomo@users.noreply.github.com>
2024-09-01 06:08:34 +0900

    Initial commit

---
+++ create_reposit.bat
@@ -0,0 +1,7 @@
+echo "# kawaiicalc" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/akiramaetomo/kawaiicalc.git
+git push -u origin main
